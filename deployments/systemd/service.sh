5f207b1dc8090381142b3cbc38b1a940636eac82
Kevin Klues <kklues@nvidia.com>
2021-03-10 15:25:18 +0000

    Update indent whitespace in scripts to tabs
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- deployments/systemd/service.sh
+++ deployments/systemd/service.sh
@@ -32,9 +32,9 @@ nvidia-mig-parted assert --mode-only
 # If it is not, then go through the process of applying it
 if [ "${?}" != 0 ]; then
 	# Apply MIG mode, without issuing a GPU reset
-    nvidia-mig-parted apply --mode-only --skip-reset
+	nvidia-mig-parted apply --mode-only --skip-reset
 	if [ "${?}" != 0 ]; then
-    	(set +x; echo "Error applying MIG mode")
+		(set +x; echo "Error applying MIG mode")
 		exit 1
 	fi
 
@@ -43,18 +43,18 @@ if [ "${?}" != 0 ]; then
 	# still not applied after reboot, this script will error out.
 	nvidia-mig-manager::service::assert_gpu_reset_available
 	if [ "${?}" != 0 ]; then
-    	(set +x;
+		(set +x;
 		echo "GPU reset capabilities are not available"
-    	echo "Attempting reboot")
+		echo "Attempting reboot")
 		nvidia-mig-manager::service::reboot
 		exit "${?}"
 	fi
 
- 	# Since the desired MIG mode is already applied, the
+	# Since the desired MIG mode is already applied, the
 	# following will just do a GPU reset under the hood
-    nvidia-mig-parted apply --mode-only
+	nvidia-mig-parted apply --mode-only
 	if [ "${?}" != 0 ]; then
-    	(set +x; echo "Error issuing GPU reset")
+		(set +x; echo "Error issuing GPU reset")
 		exit 1
 	fi
 fi
@@ -65,7 +65,7 @@ nvidia-mig-manager::service::clear_reboot_state
 
 nvidia-mig-manager::service::assert_module_loaded "nvidia"
 if [ "${?}" != 0 ]; then
-    (set +x; echo "No nvidia module loaded, skipping MIG device config")
+	(set +x; echo "No nvidia module loaded, skipping MIG device config")
 	exit 0
 fi
 
