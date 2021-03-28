d37001cc1930ba1ac0787093ab0ed99ecaca0788
Kevin Klues <kklues@nvidia.com>
2021-03-28 13:38:48 +0000

    Update deployments/systemd to make use of new hooks infrastructure
    As part of this, now start setting the CONFIG_FILE and HOOK_FILE under
    /etc/profile.d/mig-parted.sh, so that when this service is installed,
    the default config file and hooks file come from the service.
    
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

---
+++ deployments/systemd/mig-parted.sh
@@ -0,0 +1,2 @@
+export MIG_PARTED_CONFIG_FILE=/etc/nvidia-mig-manager/config.yaml
+export MIG_PARTED_HOOKS_FILE=/etc/nvidia-mig-manager/hooks.yaml
--- deployments/systemd/utils.sh
+++ deployments/systemd/utils.sh
@@ -14,10 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CURRDIR="$(cd "$( dirname $(readlink -f "${BASH_SOURCE[0]}"))" >/dev/null 2>&1 && pwd)"
-
-source ${CURRDIR}/utils-custom.sh
-
 function nvidia-mig-manager::service::reverse_array() {
 	# first argument is the array to reverse
 	# second is the reversed array
@@ -26,33 +22,6 @@ function nvidia-mig-manager::service::reverse_array() {
 	for i in "${arr[@]}"; do
 		rev=("${i}" "${rev[@]}")
 	done
-}
-
-function nvidia-mig-manager::service::assert_module_loaded() {
-	local module="${1}"
-	cat /proc/modules | grep -e "^${module} "
-	if [ "${?}" == "0" ]; then
-		return 0
-	fi
-	return 1
-}
-
-function nvidia-mig-manager::service::assert_gpu_reset_available() {
-	local devices_path="/sys/bus/pci/devices"
-	for d in $(ls "${devices_path}"); do
-		local vendor="$(cat "${devices_path}/${d}/vendor")"
-		if [ "${vendor}" != "0x10de" ]; then
-			continue
-		fi
-		local class="$(cat "${devices_path}/${d}/class")"
-		if [ "${class}" != "0x030200" ]; then
-			continue
-		fi
-		if [ ! -f "${devices_path}/${d}/reset" ]; then
-			return 1
-		fi
-	done
-	return 0
 }
 
 function nvidia-mig-manager::service::reboot() {
@@ -116,44 +85,6 @@ function nvidia-mig-manager::service::stop_systemd_services() {
 	return 0
 }
 
-function nvidia-mig-manager::service::remove_modules() {
-	local -n __modules="${1}"
-	for m in ${__modules[@]}; do
-		nvidia-mig-manager::service::assert_module_loaded "${m}"
-		if [ "${?}" != "0" ]; then
-			continue
-		fi
-		rmmod "${m}"
-		if [ "${?}" != "0" ]; then
-			return 1
-		fi
-	done
-	return 0
-}
-
-function nvidia-mig-manager::service::insert_modules() {
-	local -n __modules="${1}"
-	for m in ${__modules[@]}; do
-		modprobe ${m}
-		if [ "${?}" != "0" ]; then
-			return 1
-		fi
-	done
-	return 0
-}
-
-function nvidia-mig-manager::service::kill_k8s_containers_via_runtime_by_image() {
-	nvidia-mig-manager::service::kill_k8s_containers_via_docker_by_image "${1}"
-	if [ "${?}" != "0" ]; then
-		return 1
-	fi
-	nvidia-mig-manager::service::kill_k8s_containers_via_containerd_by_image "${1}"
-	if [ "${?}" != "0" ]; then
-		return 1
-	fi
-	return 0
-}
-
 function nvidia-mig-manager::service::kill_k8s_containers_via_docker_by_image() {
 	local images=()
 	local -n __image_names="${1}"
@@ -206,127 +137,4 @@ function nvidia-mig-manager::service::kill_k8s_containers_via_containerd_by_imag
 	done
 
 	return 0
-}
-
-function nvidia-mig-manager::service::apply_mode() {
-	local config_file="${1}"
-	local selected_config="${2}"
-
-	nvidia-mig-parted assert --mode-only -f "${config_file}" -c "${selected_config}"
-	if [ "${?}" == "0" ]; then
-		nvidia-mig-manager::service::post_apply_mode
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error running post-apply-mode")
-			return 1
-		fi
-		return 0
-	fi
-
-	local attempt=0
-	local total=10
-	(set +x; echo "Attempting to apply MIG mode setting with GPU reset (will try up to ${total} times)")
-	until [ ${attempt} -ge ${total} ]; do
-		attempt=$((attempt+1))
-		(set +x; echo "Attempt ${attempt} of ${total}")
-
-		if [ "${attempt}" != 1 ]; then
-			sleep "$((attempt*30))"
-		fi
-
-		nvidia-mig-manager::service::assert_gpu_reset_available
-		if [ "${?}" != "0" ]; then
-			nvidia-mig-parted apply --mode-only --skip-reset -f "${config_file}" -c "${selected_config}"
-			if [ "${?}" != "0" ]; then
-				(set +x; echo "There was an error setting the desired MIG mode to pending")
-				continue
-			fi
-
-			(set +x;
-			echo "There is no GPU reset available to complete the MIG mode change"
-			echo "A reboot is required to apply it")
-
-			return 1
-		fi
-
-		nvidia-mig-manager::service::pre_apply_mode
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error running pre-apply-mode")
-			continue
-		fi
-
-		nvidia-mig-parted apply --mode-only -f "${config_file}" -c "${selected_config}"
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error resetting the GPUs to activate the desired MIG mode")
-			continue
-		fi
-
-		nvidia-mig-manager::service::post_apply_mode
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error running post-apply-mode")
-			continue
-		fi
-
-		return 0
-	done
-
-	return 1
-}
-
-function nvidia-mig-manager::service::apply_config() {
-	local config_file="${1}"
-	local selected_config="${2}"
-
-	nvidia-mig-parted assert --mode-only -f "${config_file}" -c "${selected_config}"
-	if [ "${?}" != "0" ]; then
-		(set +x; echo "The MIG mode of the desired config must already be applied when running this script")
-		return 1
-	fi
-
-	nvidia-mig-manager::service::assert_module_loaded "nvidia"
-	if [ "${?}" != "0" ]; then
-		(set +x: echo "The nvidia module must be loaded to apply a MIG config with this script")
-		return 1
-	fi
-
-	nvidia-mig-parted assert -f "${config_file}" -c "${selected_config}"
-	if [ "${?}" == "0" ]; then
-		nvidia-mig-manager::service::post_apply_config
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error running post-apply-config")
-			return 1
-		fi
-		return 0
-	fi
-
-	local attempt=0
-	local total=10
-	(set +x; echo "Attempting to apply MIG config (will try up to ${total} times)")
-	until [ ${attempt} -ge ${total} ]; do
-		attempt=$((attempt+1))
-		(set +x; echo "Attempt ${attempt} of ${total}")
-
-		if [ "${attempt}" != 1 ]; then
-			sleep "$((attempt*30))"
-		fi
-
-		nvidia-mig-manager::service::pre_apply_config
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error running pre-apply-config")
-			continue
-		fi
-		nvidia-mig-parted apply -f "${config_file}" -c "${selected_config}"
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error applying the desired MIG config")
-			continue
-		fi
-		nvidia-mig-manager::service::post_apply_config
-		if [ "${?}" != "0" ]; then
-			(set +x; echo "There was an error running post-apply-config")
-			continue
-		fi
-
-		return 0
-	done
-
-	return 1
 }
