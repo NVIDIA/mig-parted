305171400723d9316bfc30733af212263e94b268
Christopher Desiniotis <cdesiniotis@nvidia.com>
2023-07-12 11:02:42 -0700

    Bump version to 0.5.3
    Signed-off-by: Christopher Desiniotis <cdesiniotis@nvidia.com>

--- cmd/nvidia-mig-parted/main.go
+++ cmd/nvidia-mig-parted/main.go
@@ -43,7 +43,7 @@ func main() {
 	c.UseShortOptionHandling = true
 	c.EnableBashCompletion = true
 	c.Usage = "Manage MIG partitions across the full set of NVIDIA GPUs on a node"
-	c.Version = "0.5.2"
+	c.Version = "0.5.3"
 
 	// Setup the flags for this command
 	c.Flags = []cli.Flag{
